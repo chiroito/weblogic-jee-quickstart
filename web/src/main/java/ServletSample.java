7e4bbb30fd00a55d3be4296c15093fa39f5e42fa
chiroito <chiroito107@gmail.com>
2017-02-04 16:51:45 +0900

    First commit

---
+++ web/src/main/java/ServletSample.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloworld", urlPatterns = {"/helloworld"})
+public class ServletSample extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("Hello World!");
+    }
+}
